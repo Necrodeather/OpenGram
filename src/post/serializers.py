5396718a0b00a7c728f57970f562ecd63f5c55a1
Necrodeather <Morbid6dead@gmail.com>
2023-09-11 21:28:19 +0300

    fixed get posts

--- src/post/serializers.py
+++ src/post/serializers.py
@@ -1,10 +1,35 @@
+from uuid import uuid4
+
 from rest_framework import serializers
 
-from post.models import Post
+from post.models import Image, Post
 from user.models import CustomUser
 
 
+class ImagePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ["id", "image"]
+
+
 class CreatePostSerializer(serializers.ModelSerializer):
+    images = serializers.ListField(
+        child=serializers.ImageField(),
+    )
+
+    def create(self, validated_data):
+        post = Post.objects.create(
+            description=validated_data["description"],
+            user=validated_data["user"],
+        )
+        images = []
+        for image in validated_data["images"]:
+            image.name = str(uuid4())
+            images.append(Image(post=post, image=image))
+        Image.objects.bulk_create(images, batch_size=10)
+        post.images = images
+        return post
+
     class Meta:
         model = Post
         fields = ["images", "description"]
@@ -18,8 +43,8 @@ class UserPostSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     user = UserPostSerializer(read_only=True)
-    images = serializers.ListField(
-        child=serializers.ImageField(),
+    images = serializers.ListSerializer(
+        child=ImagePostSerializer(),
     )
 
     class Meta:
